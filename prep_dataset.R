f52170bc27716c22074723beffa360e34854ac21
Nico Blokker <blokker@uni-bremen.de>
2021-11-10 12:56:33 +0100

    deployed app

--- prep_dataset.R
+++ prep_dataset.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(lubridate)
 
-data <- mardyr2:::LRE %>% filter(cdate >= "2015-09-01" & cdate <= "2015-09-14") %>% select(-legacy)
+data <- mardyr2:::LRE %>% filter(cdate >= "2015-09-01" & cdate <= "2015-09-07") %>% select(-legacy)
 migration_codebook_english <- mardyr2:::migration_codebook_english
 data$cdate <- lubridate::ymd(data$cdate)
 data$parties[data$parties == "AFD"] <- "AfD"
@@ -9,6 +9,6 @@ data$parties <- gsub("Gr.ne", "Green", data$parties)
 data$parties[data$parties == "Linke"] <- "Left"
 data <- data %>% dplyr::mutate(rowid = paste0("C", 1:nrow(.)))
 
+saveRDS(list(data, migration_codebook_english), "./demo/mardy_data.rds")
 
-write.csv(data, "mardy_data.csv", fileEncoding = "UTF-8", row.names = F)
-write.csv(migration_codebook_english, "mardy_codebook.csv", fileEncoding = "UTF-8", row.names = F)
+
